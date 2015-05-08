4821773b5f7fc118d137c19b8d3dfcadb67e6cb7
Mendel Schneerson & Michael Bozza <pair+benlights.michaelbbozza@devbootcamp.com>
2015-05-08 15:30:02 -0400

    finished deck model

--- models/deck.rb
+++ models/deck.rb
@@ -1,25 +1,41 @@
 class Deck
+  attr_reader :cards
+  attr_accessor :current_card
   def initialize(args = {})
     @cards = args[:cards]
+    @current_card = nil
   end
 
-  def pick_card
-
+  def pick_card(card_array)
+    current_array = card_array.shuffle!
+    current_card = current_array.first
   end
 
-  def answered!
+  def show_answer(card)
+    card.answer
+  end
 
+  def show_question(card)
+    card.question
+  end
+
+  def answered!(card)
+    card.answer_state = true
+  end
+
+  def correct_answer?(card, input)
+    card.answer == input
   end
 
   def show_unanswered
-
+    cards.reject {|card| card.answer_state}
   end
 
   def show_answered
-
+    cards.select {|card| card.answer_state}
   end
 
   def all_answered?
-
+    cards.all? {|c| c.answer_state}
   end
 end
