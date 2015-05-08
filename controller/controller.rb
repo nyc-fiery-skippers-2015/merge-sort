bc900e5396c21977dffeff6947c1654c29d5beb0
Mendel Schneerson & Michael Bozza <pair+benlights.michaelbbozza@devbootcamp.com>
2015-05-08 17:27:51 -0400

    added decks

--- controller/controller.rb
+++ controller/controller.rb
@@ -5,11 +5,15 @@ require_relative '../modules/parser.rb'
 
 require 'pry'
 class GameController
-  attr_reader :deck
+  attr_reader :decks_array
+  attr_accessor :deck
   def initialize
     # binding.pry
-    @deck = Deck.new(Parser.parse_text_file('flashcard_samples.txt'))
+    @decks_array = [Deck.new(Parser.parse_text_file('flashcard_samples.txt')),
+                  Deck.new(Parser.parse_text_file('animal_cards.txt'))]
+    @deck = decks_array[0]
   end
+
 
   def start
     View.welcome
@@ -17,7 +21,17 @@ class GameController
   end
 
   def run
-    input = ""
+
+    input = View.input
+    case input
+    when '1'
+      deck = decks_array[0]
+    when '2'
+      deck = decks_array[1]
+    else
+      View.deck_not_available
+    end
+
     until input == 'quit' || deck.all_answered?
       card = deck.pick_card(deck.show_unanswered)
       View.display(deck.show_question(card))
